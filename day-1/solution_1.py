060179160d94467c630bb359c5fe59ace2458908
Adam Sroka <git@adam.sr>
2022-12-01 18:06:31 +0000

    Add first day part two solution

--- day-1/solution_1.py
+++ day-1/solution_1.py
@@ -1,4 +1,3 @@
-
 def get_all_elves_cal():
     with open("./input.txt", "r") as input_data:
         all_elves_calories = list()
@@ -12,18 +11,29 @@ def get_all_elves_cal():
     return all_elves_calories
 
 
+def sum_each_elfs_calories(all_elves_calories):
+    return [sum(elf_calories) for elf_calories in all_elves_calories]
+
+
 def find_max_total_cal(all_elves_calories):
-    return max([sum(elf_calories) for elf_calories in all_elves_calories])
+    return max(sum_each_elfs_calories(all_elves_calories))
 
 
-def write_solution(solution):
-    with open("./solution.txt", "w") as solution_data:
-        solution_data.write(str(solution))
-        solution_data.write("\n")
+def find_top_three_max_total_cal(all_elves_calories):
+    sorted_total_cals = sorted(sum_each_elfs_calories(all_elves_calories), reverse=True)
+    return sum(sorted_total_cals[:3])
+
+
+def write_answers(answers, path="./answer.txt"):
+    with open(path, "w") as answer_data:
+        for answer in answers:
+            answer_data.write(str(answer))
+            answer_data.write("\n")
 
 
 if __name__ == "__main__":
     all_elves_calories = get_all_elves_cal()
     max_total_cal = find_max_total_cal(all_elves_calories)
-    write_solution(max_total_cal)
-    print(max_total_cal)
+    top_three_max_total_cal = find_top_three_max_total_cal(all_elves_calories)
+    write_answers((max_total_cal, top_three_max_total_cal))
+    print(max_total_cal, top_three_max_total_cal)
