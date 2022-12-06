359f9b15328c925ec5e400b1ebc80363f1013c4a
Adam Sroka <git@adam.sr>
2022-12-06 10:13:21 +0000

    Add type hints to day six solution

--- day-6/solution_6.py
+++ day-6/solution_6.py
@@ -1,7 +1,7 @@
 INPUT_PATH = "./input.txt"
 
 
-def find_packet_start_index(data: str, unique_char_num=4) -> int:
+def find_packet_start_index(data: str, unique_char_num: int = 4) -> int:
     for i in range(len(data)):
         if len(set(data[i : i + unique_char_num])) == unique_char_num:
             return i + unique_char_num
@@ -12,12 +12,12 @@ def find_message_start_index(data: str) -> int:
     return find_packet_start_index(data, unique_char_num=14)
 
 
-def read_data(input_path=INPUT_PATH):
+def read_data(input_path: str = INPUT_PATH) -> str:
     with open(input_path, "r") as input_data:
         return input_data.readlines()[0]
 
 
-def write_answers(answers: list, path="./answer.txt") -> None:
+def write_answers(answers: list, path: str = "./answer.txt") -> None:
     with open(path, "w") as answer_data:
         for answer in answers:
             answer_data.write(str(answer))
